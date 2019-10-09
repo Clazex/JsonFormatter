7bb5ec5f7892960afc973ec4fcb51ca5d7a55e57
Sciencmine <2631197967@qq.com>
2019-10-09 19:52:39 +0800

    Update JsonFormatter.cpp
    Use ostream
    Add more tip info
    Use macro for constants
    bugfix

--- JsonFormatter.cpp
+++ JsonFormatter.cpp
@@ -7,29 +7,35 @@
 #include <fstreamHelper.h>
 
 using std::cin;
+using std::cout;
+using std::endl;
 
+#define ESC_SLEEPING_TIME (100)
 #define ERR_SLEEPING_TIME (750)
 
 int main (int argc, char **argv) {
+	std::ios::sync_with_stdio (false);
+
 	if (argc > 1) {
+		cout << argv[0] << " > " << argv[1];
+
 		bool state = true;
 		file_buffer file (argv[1]);
 		if (!state) {
-			printf ("\nAn Error Has Occured When Reading The File!\n\n");
+			cout << "\nAn Error Has Occured When Reading The File!\n" << endl;
 			system ("pause");
 		}
 
 		doc doc;
 		doc.Parse (file.getBuffer (), file.getSize ());
-		file.~file_buffer ();
 		if (doc.HasParseError ()) {
-			printf ("\nAn Error Has Occured When Parsing The JSON!\n\n");
+			cout << "\nAn Error Has Occured When Parsing The JSON!\n" << endl;
 			system ("pause");
 		}
 
 		ofstream fout;
 		if (!fstreamHelper::topen (fout, argv[1], ios_base::trunc | ios_base::out)) {
-			printf ("\nAn Error Has Occured When Writing The File!\n\n");
+			cout << "\nAn Error Has Occured When Writing The File!\n" << endl;
 			system ("pause");
 		}
 
@@ -37,23 +43,27 @@ int main (int argc, char **argv) {
 
 		fout.clear ();
 		fout.close ();
+
+		Sleep (ESC_SLEEPING_TIME);
+
 		return 0;
 	}
 
 	while (true) {
-		printf ("%s > ", argv[0]);
+		cout << argv[0] << " > ";
 		char path[_MAX_PATH];
 		cin >> path;
 
-		if (path[1] != ':') {
-			printf ("Exiting...\n", argv[0]);
+		if (path[0] == 'e' && path[1] == 'x' && path[2] == 'i' && path[3] == 't') {
+			cout << "Exiting..." << endl;
+			Sleep (ESC_SLEEPING_TIME);
 			break;
 		}
 
 		bool state = true;
 		file_buffer file (path, state);
 		if (!state) {
-			printf ("\nAn Error Has Occured When Reading The File!\n\n");
+			cout << "\nAn Error Has Occured When Reading The File!\n" << endl;
 			Sleep (ERR_SLEEPING_TIME);
 			continue;
 		}
@@ -61,18 +71,17 @@ int main (int argc, char **argv) {
 		doc doc;
 		doc.Parse (file.getBuffer (), file.getSize ());
 		if (doc.HasParseError ()) {
-			printf ("\nAn Error Has Occured When Parsing The JSON!\n\n");
+			cout << "\nAn Error Has Occured When Parsing The JSON!\n" << endl;
 			Sleep (ERR_SLEEPING_TIME);
 			continue;
 		}
 
 		ofstream fout;
 		if (!fstreamHelper::topen (fout, path, ios_base::trunc | ios_base::out)) {
-			printf ("\nAn Error Has Occured When Writing The File!\n\n");
+			cout << "\nAn Error Has Occured When Writing The File!\n" << endl;
 			Sleep (ERR_SLEEPING_TIME);
 			continue;
 		}
-
 
 		fout << jsonHelper::stringify (doc);
 
